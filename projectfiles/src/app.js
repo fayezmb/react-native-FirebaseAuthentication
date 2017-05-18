3721e5f91620b54211919ccab47f7bb674305ff7
fayezmb <fayezmb@hotmail.com>
2017-05-18 13:46:23 +0400

    added old components

--- projectfiles/src/app.js
+++ projectfiles/src/app.js
@@ -5,7 +5,6 @@ import {
   Text,
   View
 } from 'react-native';
-import {Header} from './components/common';
 class App extends Component {
   render(){
     return (
---
+++ projectfiles/src/components/common/index.js
@@ -0,0 +1,4 @@
+export * from '/Button';
+export * from '/Card';
+export * from '/header';
+export * from '/CardSection';
