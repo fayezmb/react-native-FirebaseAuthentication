885ebd4003cb835b3addead72335c7f4f82ffdec
fayezmb <fayezmb@hotmail.com>
2017-05-18 18:57:31 +0400

    stuff

--- projectfiles/app/app.js
+++ projectfiles/app/app.js
@@ -6,7 +6,19 @@ import {
   View
 } from 'react-native';
 import {Header} from './components/common';
+import firebase from 'firebase';
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      {
+    apiKey: "AIzaSyDtE7Z_bH3j0UfsuLk19QuZ8LuIkSM_j0I",
+    authDomain: "fir-authtest-c78fd.firebaseapp.com",
+    databaseURL: "https://fir-authtest-c78fd.firebaseio.com",
+    projectId: "fir-authtest-c78fd",
+    storageBucket: "fir-authtest-c78fd.appspot.com",
+    messagingSenderId: "619116072113"
+  });
+  }
   render(){
     return (
       <View>
