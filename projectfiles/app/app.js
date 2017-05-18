21134ce55ff802f88985906ddd890a4a5a02e769
fayezmb <fayezmb@hotmail.com>
2017-05-18 12:33:16 +0400

    project initiation

---
+++ projectfiles/app/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+class App extends Component {
+  render(){
+    return (
+      <View>
+      <Text> Hello </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
