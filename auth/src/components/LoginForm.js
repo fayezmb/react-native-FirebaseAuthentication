a745076218a9241ca9dadb3ae2148f376dcf60f3
fayezmb <fayezmb@hotmail.com>
2017-05-20 16:53:55 +0400

    update
    update

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -4,7 +4,40 @@ import firebase from 'firebase';
 import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: ''};
+  state = { email: '', password: '', error: '', loading: false};
+  onButtonPress(){
+    const {email, password}= this.state;
+    this.setState({error: '', loading: true})
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then(this.onLoginSuccess.bind(this))
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email,password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(this.onLoginFail.bind(this));
+      });
+    
+  }
+  onLoginFail(){
+    this.setState({error: 'Authentication Failed', loading: false})
+  }
+  renderButton(){
+    if(this.state.loading){
+      return <Spinner size="small" />;
+    }
+    return(
+      <Button onPress={this.onButtonPress.bind(this)}>
+      dis button
+      </Button>
+    );
+  }
+  onLoginSuccess(){
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+  }
   render() {
     return (
       <Card>
@@ -25,13 +58,20 @@ class LoginForm extends Component {
       onChangeText={password => this.setState({password})}
       />
       </CardSection>
+      <Text style={styles.errorTextStyle}>
+      {this.state.error}
+      </Text>
       <CardSection>
-      <Button>
-      dis Button
-      </Button>
       </CardSection>
       </Card>
     );
   }
   }
+  const styles= {
+    errorTextStyle: {
+      fontSize: 20,
+      alignSelf: 'center',
+      color: 'red'
+    }
+  };
 export default LoginForm;
