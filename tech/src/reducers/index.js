ecdea669d926a1877c92d917df920d31c5c0da6c
fayezmb <fayezmb@hotmail.com>
2017-05-29 15:54:25 +0400

    more reducer work

--- tech/src/reducers/index.js
+++ tech/src/reducers/index.js
@@ -1,5 +1,7 @@
 import libraryreducer from './libraryreducer';
 import {combineReducers} from 'redux';
+import SelectionReducer from './SelectionReducer';
 export default combineReducers ({
-  libraries: libraryreducer
+  libraries: libraryreducer,
+  selectedLibraryId: SelectionReducer
 });
